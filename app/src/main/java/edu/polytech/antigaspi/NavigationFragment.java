4d10231960ce85926172f829cd0ef81e3510ee82
Léo Kitabdjian <leo.kitabdjian@etu.unice.fr>
2022-03-30 16:06:38 +0200

    Premier fragment navigation bar

---
+++ app/src/main/java/edu/polytech/antigaspi/NavigationFragment.java
@@ -0,0 +1,24 @@
+package edu.polytech.antigaspi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NavigationFragment extends Fragment {
+
+    public NavigationFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.navigation_fragment, container, false);
+        return view;
+    }
+}
