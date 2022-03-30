780cdc584f23071fcd64c377db47c8b85cdccc59
Léo Kitabdjian <leo.kitabdjian@etu.unice.fr>
2022-03-30 17:14:02 +0200

    fragment navig bar

--- app/src/main/java/edu/polytech/antigaspi/NavigationFragment.java
+++ app/src/main/java/edu/polytech/antigaspi/NavigationFragment.java
@@ -5,20 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class NavigationFragment extends Fragment {
 
     public NavigationFragment() {
-
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.navigation_fragment, container, false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.navigation_fragment, container, false);
     }
 }
