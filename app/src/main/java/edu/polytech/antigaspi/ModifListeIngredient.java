ef93c48465261950697e0fa6c409fcc49b505088
Nolwenn <nolwenn.peyratout@etu.univ-cotedazur.fr>
2022-03-30 15:50:26 +0200

    #9 création des différentes vues

---
+++ app/src/main/java/edu/polytech/antigaspi/ModifListeIngredient.java
@@ -0,0 +1,14 @@
+package edu.polytech.antigaspi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModifListeIngredient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modif_liste_ingredient);
+    }
+}
---
+++ app/src/main/java/edu/polytech/antigaspi/RechercheAstuce.java
@@ -0,0 +1,14 @@
+package edu.polytech.antigaspi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RechercheAstuce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recherche_astuce);
+    }
+}
