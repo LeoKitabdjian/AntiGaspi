e605c5a0aa9e35a906b95c234ed8c8dbf9a652c9
Val <valentin.podda@etu.unice.fr>
2022-04-27 15:31:45 +0200

    correction sucrée

--- app/src/main/java/edu/polytech/antigaspi/Tips.java
+++ app/src/main/java/edu/polytech/antigaspi/Tips.java
@@ -13,8 +13,8 @@ public class Tips extends ArrayList<Tip> {
         SweetFactory sweetFactory = new SweetFactory();
         add(new Tip(saltFactory.buildIngredient("Fromage"), "Pensez à bien le mettre au frais pour le conserver plus longtemps"));
         add(new Tip(saltFactory.buildIngredient("Couscous"), "Pensez à bien le conserver dans un récipient fermé et un endroit sec"));
-        add(new Tip(saltFactory.buildIngredient("Pomme"), "Si vous mettez une pomme au frigo, elle va noircir mais vous pouvez toujours la manger tant qu'elle n'est pas abimée"));
-        add(new Tip(saltFactory.buildIngredient("Banane"), "Vous pouvez manger une banane même si celle-ci a noirci"));
+        add(new Tip(sweetFactory.buildIngredient("Pomme"), "Si vous mettez une pomme au frigo, elle va noircir mais vous pouvez toujours la manger tant qu'elle n'est pas abimée"));
+        add(new Tip(sweetFactory.buildIngredient("Banane"), "Vous pouvez manger une banane même si celle-ci a noirci"));
 
     }
 
