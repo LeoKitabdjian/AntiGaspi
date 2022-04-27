256ef8a83abddce1bda02344ea5337dc9ccf855d
Nolwenn <nolwenn.peyratout@etu.univ-cotedazur.fr>
2022-04-27 15:28:47 +0200

    #7 Modification des notifs avec images

--- app/src/main/java/edu/polytech/antigaspi/mainActivities/MainActivity.java
+++ app/src/main/java/edu/polytech/antigaspi/mainActivities/MainActivity.java
@@ -7,7 +7,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
-import edu.polytech.antigaspi.AjoutIngredientFrigo;
+import edu.polytech.antigaspi.Astuce;
 import edu.polytech.antigaspi.R;
 
 public class MainActivity extends ActivitesPrincipales {
@@ -20,7 +20,13 @@ public class MainActivity extends ActivitesPrincipales {
         createLinks();
         requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
         handleGPS();
-
+        findViewById(R.id.notif).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Astuce.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void handleGPS() {
