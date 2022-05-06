a2f6aab127ab90d50a888d9748391afbe7c3a9a7
ChatelainClement <clementchatelain@hotmail.fr>
2022-05-06 15:07:51 +0200

    Ajout de communication avec l'Agenda

--- app/src/main/java/edu/polytech/antigaspi/AjoutIngredientFrigo.java
+++ app/src/main/java/edu/polytech/antigaspi/AjoutIngredientFrigo.java
@@ -1,14 +1,21 @@
 package edu.polytech.antigaspi;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AjoutIngredientFrigo extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+public class AjoutIngredientFrigo extends AppCompatActivity {
+    public Calendar calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,38 @@ public class AjoutIngredientFrigo extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        calendar= Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        int year = calendar.get(Calendar.YEAR);
+        int month  = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        ImageView button = (ImageView) findViewById(R.id.logoAgenda);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        setTime(year,monthOfYear,dayOfMonth);
+                        long time = calendar.getTime().getTime();
+                        Intent intent = new Intent(Intent.ACTION_EDIT);
+                        intent.setType("vnd.android.cursor.item/event");
+                        intent.putExtra("beginTime",time);
+                        intent.putExtra("allDay", true);
+                        intent.putExtra("rule", "FREQ=YEARLY");
+                        intent.putExtra("endTime", time+60*60*1000);
+                        intent.putExtra("title", "A Test Event from android app");
+                        startActivity(intent);
+                    }
+                };
+                DatePickerDialog datePickerDialog = new DatePickerDialog(view.getContext(),
+                        dateSetListener, year, month, day);
+                datePickerDialog.show();
+
+            }
+        });
+    }
+    public void setTime(int year, int month, int dayOfMonth){
+        calendar = new GregorianCalendar(year, month, dayOfMonth);
     }
 }
