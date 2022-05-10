6c6ec4463cbee7155e0c988645026901e62248b3
Léo K <leo.kitabdjian@etu.unice.fr>
2022-05-10 18:48:06 +0200

    rajouter un ingrédient

--- app/src/main/java/edu/polytech/antigaspi/AjoutIngredientFrigo.java
+++ app/src/main/java/edu/polytech/antigaspi/AjoutIngredientFrigo.java
@@ -1,11 +1,15 @@
 package edu.polytech.antigaspi;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,23 +19,33 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import edu.polytech.antigaspi.fragments.PhotoFragment;
+import edu.polytech.antigaspi.ingredients.SaltIngredient;
+import edu.polytech.antigaspi.mainActivities.MonFrigo;
+import edu.polytech.antigaspi.recipes.RecipeComponent;
+import edu.polytech.antigaspi.recipes.RecipeComponents;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class AjoutIngredientFrigo extends AppCompatActivity {
     private PhotoFragment photoFragment;
-    public Calendar calendar;
+    private Calendar calendar;
+    private SharedPreferences prefs;
+    private EditText quantity;
+    private EditText name;
+    private EditText date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ajout_ingredient_frigo);
+        prefs = this.getSharedPreferences("add_ingredient", Context.MODE_PRIVATE);
+        quantity = (EditText) findViewById(R.id.add_ingredient_quantity);
+        name = (EditText) findViewById(R.id.add_ingredient_name);
+        date = (EditText) findViewById(R.id.add_ingredient_date);
         Intent intent = getIntent();
-        String date = intent.getStringExtra("date");
-        TextView msg = findViewById(R.id.DateP);
-        msg.setText(date);
-        View frag= findViewById(R.id.photoFrag);
-
+        String dateFromPhoto = intent.getStringExtra("date");
+        date.setText(dateFromPhoto);
         findViewById(R.id.logoPhoto).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,8 +83,38 @@ public class AjoutIngredientFrigo extends AppCompatActivity {
 
             }
         });
+        handleSaveButton();
     }
+
+    private void handleSaveButton() {
+        ((Button) findViewById(R.id.confirmButton)).setOnClickListener(view -> {
+            if (name.getText().toString().length() != 0 && quantity.getText().toString().length() != 0 && date.getText().toString().length() != 0) {
+                prefs.edit().remove("Ingredient name").apply();
+                prefs.edit().remove("Ingredient quantity").apply();
+                RecipeComponents.getInstance().add(new RecipeComponent(new SaltIngredient(name.getText().toString()),
+                        Integer.parseInt(quantity.getText().toString())));
+                Intent intent = new Intent(AjoutIngredientFrigo.this, MonFrigo.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        prefs.edit().putString("Ingredient name", name.getText().toString()).apply();
+        prefs.edit().putString("Ingredient quantity", quantity.getText().toString()).apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        name.setText(prefs.getString("Ingredient name", ""));
+        quantity.setText(prefs.getString("Ingredient quantity", ""));
+    }
+
     public void setTime(int year, int month, int dayOfMonth){
         calendar = new GregorianCalendar(year, month, dayOfMonth);
     }
+
 }
